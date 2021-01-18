afbec7a1b59431a89b7cf139521e06e674c648d7
r-fujikura <r-fujikura@sumilab.org>
2021-01-18 21:50:24 +0900

    突っ込みどころさん

--- test_code_java.java
+++ test_code_java.java
@@ -2,6 +2,15 @@ public class SampleJava {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
-  }
+    
+    private int date = 2;
 
+
+    if( date = 1 ){
+    System.out.println("I'm sleeping!");
+    }
+
+    if( date = 2 ){
+    }
+  }
 }
